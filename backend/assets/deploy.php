bf62f28e8983711eb6cb610602dbc9af8daf46be
陈小龙 <13480731740@qq.com>
2016-12-18 22:18:57 +0800

    数据库数据配置

--- backend/assets/deploy.php
+++ backend/assets/deploy.php
@@ -1,6 +1,6 @@
 <?php
-    define("SITE_HOME_URL","http://www.baobei.skip.pw");
-    define("SITE_ADMIN_URL","http://admin.baobei.skip.pw");
+    define("SITE_HOME_URL","http://baobei.dai12345.com");
+    define("SITE_ADMIN_URL","http://adbb.dai12345.com");
     define("STYLE_URL",SITE_ADMIN_URL."/statics/css/");
     define("IMG_URL",SITE_ADMIN_URL."/statics/images/");
     define("JS_URL",SITE_ADMIN_URL."/statics/js/");
